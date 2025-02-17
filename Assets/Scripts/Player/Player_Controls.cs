4fb89afb27e910143dcdc45f3044e0328acc2bde
MostAmbitiousCrow <sammywang54@gmail.com>
2025-02-17 23:17:03 +0000

    Initial Migration
    Migrated to a Unity URP 3D project due to 2D limitations with 3D elements, such as materials.

--- Assets/Scripts/Player/Player_Controls.cs
+++ Assets/Scripts/Player/Player_Controls.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-// Made by Samuel
-
 public class Player_Controls : MonoBehaviour
 {
     [Header("Player Stats")]
