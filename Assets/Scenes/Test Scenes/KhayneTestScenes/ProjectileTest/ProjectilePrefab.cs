dbd00ce5af4cff3de093cde1d628cec1c7deb8b7
SilentVortex <149170463+SilentVortex@users.noreply.github.com>
2025-02-18 00:08:24 +0000

    got my test scenes back up, just had to replace sprites since migration removed default unity 2D sprites :(

---
+++ Assets/Scenes/Test Scenes/KhayneTestScenes/ProjectileTest/ProjectilePrefab.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectilePrefab : MonoBehaviour // Made by Khayne
+{
+    [SerializeField] float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
