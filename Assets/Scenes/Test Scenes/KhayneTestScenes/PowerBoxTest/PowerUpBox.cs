f129698d8fe080cb8538fe6883c7dfad9484bf16
SilentVortex <149170463+SilentVortex@users.noreply.github.com>
2025-02-23 19:12:28 +0000

    we dont look at the mega scuffed script, script made in other words but still wip

--- Assets/Scenes/Test Scenes/KhayneTestScenes/PowerBoxTest/PowerUpBox.cs
+++ Assets/Scenes/Test Scenes/KhayneTestScenes/PowerBoxTest/PowerUpBox.cs
@@ -1,21 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class PowerUpBox : MonoBehaviour // Made by Khayne. This script is for the power up box that the player can destroy to get a power up.
 {
-    [SerializeField] private GameObject prefab;
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("PlayerProjectile"))
-        {
-            BreakOpen();
-        }
-    }
+    [SerializeField] private List<GameObject> PowerUps;
+    [SerializeField] private GameObject SetPowerUp;
+    [SerializeField] private bool Assigned = false;
 
-    [SerializeField] void BreakOpen()
+    public void BreakOpen()
     {
-        Instantiate(prefab, transform.position, transform.rotation);
         Destroy(gameObject);
+        if (Assigned)
+        {
+            Instantiate(SetPowerUp, transform.position, Quaternion.identity);
+        }
+        else
+        {
+            int RandomPowerUp = Random.Range(0, PowerUps.Count);
+            Instantiate(PowerUps[RandomPowerUp], transform.position, Quaternion.identity);
+        }
     }
 }
