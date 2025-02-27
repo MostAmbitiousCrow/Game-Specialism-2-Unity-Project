f53fc40b659a60f89016588533731bfe6460c877
MostAmbitiousCrow <sammywang54@gmail.com>
2025-02-27 17:56:34 +0000

    Squashed commit of the following:
    commit b80f118d58383a5a74f80a7b642cf7c22a2e0e0e
    Author: MostAmbitiousCrow <sammywang54@gmail.com>
    Date:   Thu Feb 27 17:55:40 2025 +0000
    
        Import Hell Environment Asset + Player Multiple Enemy Shooting
    
    commit 29176484c87b62cbe21c02ddcca2a077c859457e
    Author: MostAmbitiousCrow <sammywang54@gmail.com>
    Date:   Thu Feb 27 13:17:13 2025 +0000
    
        Debug + Shooting at "Enemy" Tagged Objects
    
    commit 7beac8bdf0da43cc7c820ca4a00d23d1203a61ed
    Author: MostAmbitiousCrow <sammywang54@gmail.com>
    Date:   Fri Feb 21 17:11:27 2025 +0000
    
        Outline Material + Player Shooting Update
    
    commit f22eab28654b85fb9cab3084912c0451f689c018
    Author: Samuel White <148637178+MostAmbitiousCrow@users.noreply.github.com>
    Date:   Thu Feb 20 22:18:00 2025 +0000
    
        Initial Player Enemy Detect Script
    
    commit 7d87573628d39e6ed37b400277c58313369ffa77
    Author: MostAmbitiousCrow <sammywang54@gmail.com>
    Date:   Thu Feb 20 14:35:37 2025 +0000
    
        Pixel and Posterize Shader
    
    commit 8db1942edd00045bfcc3816b9c221301834208fa
    Author: MostAmbitiousCrow <sammywang54@gmail.com>
    Date:   Tue Feb 18 22:44:31 2025 +0000
    
        Player Shooting Test + Layers

--- Assets/Scripts/Samuel Test Scene Scripts/Player/Player_Shoot_Flight.cs
+++ Assets/Scripts/Samuel Test Scene Scripts/Player/Player_Shoot_Flight.cs
@@ -1,12 +1,127 @@
-using System.Collections;
+using System.Reflection;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using Unity.Mathematics;
+using System.Linq;
 
-public class Player_Shoot_Flight : MonoBehaviour
+public class Player_Shoot_Flight : MonoBehaviour // By Samuel White
 {
+    [Header("Player Shoot Controls")]
+    [SerializeField] private ScriptableObject projectileData;
+    [SerializeField] private bool isShooting = false;
     [SerializeField] float fireRate = .2f;
+    private float t = 0;
+
+    [Header("Ring")]
+    [SerializeField] Transform[] enemyDetectRings;
+    [SerializeField] Material enemyDetectRingMaterial;
+
+    [Header("Enemy Detection")]
+    [Range(1, 9.9f)] [SerializeField] float detectRadius = 5;
+    [Range(1, 16)] [SerializeField] float detectRange = 10;
+
+    [Range(0, 2)] [SerializeField] int closestEnemiesRange = 2;
+
+    [SerializeField] List<Transform> detectedEnemies;
+    [SerializeField] LayerMask enemyLayer;
+
+    [Header("Debug")]
+    [SerializeField] bool enableDebug = true;
+    [SerializeField] Mesh mesh;
+    [SerializeField] bool debugAffectMaterial = false;
+
+    void Start()
+    {
+        // enemyDetectRingMaterial = enemyDetectRing.GetComponent<Renderer>().material;
+    }
+    
+    void Update()
+    {
+        Shooting();
+        DetectEnemies();
+        UpdateRings();
+    }
+
+    private void DetectEnemies()
+    {
+        detectedEnemies.Clear();
+        Vector3 pPos = transform.position;
+        Collider[] colliders = Physics.OverlapCapsule(pPos, new(pPos.x, pPos.y, pPos.z + detectRange), detectRadius / 2, enemyLayer); // https://roundwide.com/physics-overlap-capsule/
+        Debug.DrawLine(pPos, new(pPos.x, pPos.y, pPos.z + detectRange));
+
+        foreach (var item in colliders)
+        {
+            detectedEnemies.Add(item.transform);
+        }
+
+        detectedEnemies.Sort((t1, t2) => // https://discussions.unity.com/t/sorting-a-list-by-distance-to-an-object/178943/3
+        {
+            return Vector3.Distance(t1.transform.position, transform.position)
+                .CompareTo(Vector3.Distance(t2.transform.position, transform.position));
+        });
+    }
+
+    private void Shooting()
+    {
+        if (isShooting)
+        {
+            t += Time.deltaTime;
+            if (t >= fireRate)
+            {
+                Shoot();
+                t = 0;
+            }   
+        }
+    }
+
+    private void UpdateRings()
+    {
+        int c = Mathf.Clamp(detectedEnemies.Count, 0, closestEnemiesRange);
+
+        for (int i = 0; i < c; i++)
+        { enemyDetectRings[i].position = detectedEnemies[i].position; enemyDetectRings[i].gameObject.SetActive(true); }
+        if (c < closestEnemiesRange)
+        {
+            for (int i = c; i < closestEnemiesRange; i++)
+            { enemyDetectRings[i].position = new(); enemyDetectRings[i].gameObject.SetActive(false); }
+        }
+    }
+
     public void Shoot()
     {
+        for (int i = 0; i < Mathf.Clamp(detectedEnemies.Count, 0, closestEnemiesRange); i++)
+        {
+            (GameObject GO, FieldInfo SO) = Bullet_Pool_System.instance.GetBullet(0);
+            if (GO != null && SO != null)
+            {
+                Quaternion look = detectedEnemies.Count == 0 ? Quaternion.identity : Quaternion.LookRotation(detectedEnemies[i].position - transform.position);
+                GO.transform.SetPositionAndRotation(transform.position, look);
+                SO.SetValue(GO.GetComponent(SO.DeclaringType), projectileData);
+                GO.SetActive(true);
+            }
+            else
+            {
+                Debug.LogWarning("Failed to get bullet from pool.");
+            }   
+        }
+    }
 
+    public void Shooting(InputAction.CallbackContext context)
+    {
+        isShooting = context.ReadValueAsButton();
+    }
+
+    void OnDrawGizmos()
+    {
+        if (enableDebug)
+        {
+            Gizmos.color = Color.green;
+            Quaternion rot = Quaternion.identity;
+            Gizmos.DrawWireMesh(mesh, 0, new(transform.position.x, transform.position.y, transform.position.z + detectRange /2 - (detectRange * .25f)),
+                Quaternion.Euler(90, rot.y, 0), new(detectRadius, detectRange/2, detectRadius));
+            
+            if (debugAffectMaterial) enemyDetectRingMaterial.SetFloat("_Radius", detectRadius * .05f);
+        }
     }
 }
