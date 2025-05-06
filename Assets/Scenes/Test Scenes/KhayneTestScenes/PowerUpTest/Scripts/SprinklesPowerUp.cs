7fc67e9cab99e3a0d837f320bb5ba716a323c294
SilentVortex <149170463+SilentVortex@users.noreply.github.com>
2025-05-06 22:07:37 +0100

    Did some work on Sprinkles power up

--- Assets/Scenes/Test Scenes/KhayneTestScenes/PowerUpTest/Scripts/SprinklesPowerUp.cs
+++ Assets/Scenes/Test Scenes/KhayneTestScenes/PowerUpTest/Scripts/SprinklesPowerUp.cs
@@ -3,19 +3,26 @@ using UnityEngine;
 public class SprinklesPowerUp : MonoBehaviour
 {
     // This script is for the sprinkles power-up.
-    bool ready = true;
-    public void PowerUpTrigger()
+    bool ready = true; // Indicates if the power-up is ready to be used.
+    [SerializeField] GameObject SprinklesMinigun; // The sprinkles minigun prefab.
+    [SerializeField] GameObject bullet; // The sprinkles minigun prefab.
+    public void PowerUpTrigger() // Called when the power-up is triggered.
     {
-        if (ready)
+        if (ready) // Check if the power-up is ready.
         {
+            SprinklesMinigun.SetActive(true); // Activate the sprinkles minigun.
             
+            for (int i = 0; i < 60; i++) // Loop to shoot the minigun 60 times.
+            {
+
+            }
         }
     }
 
-    public void PowerUpDeactivate()
+    public void PowerUpDeactivate() // Called when the power-up is deactivated.
     {
-        // Called as to end the power-up.
-        enabled = false;
-        ready = false;
+        enabled = false; // Disable this script.
+        ready = false; // Set the power-up to not ready.
+        SprinklesMinigun.SetActive(false); // Deactivate the sprinkles minigun.
     }
 }
